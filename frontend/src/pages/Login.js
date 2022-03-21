c1dd8d29d96f651bab5aacb25a0e18309c06b868
sunfkkc <rlcks77@nate.com>
2022-03-21 19:34:58 +0900

    frontend

--- frontend/src/pages/Login.js
+++ frontend/src/pages/Login.js
@@ -1,34 +1,60 @@
 import React ,{useState, useEffect}from 'react'
-import {Link} from 'react-router-dom'
+import {Link, useNavigate} from 'react-router-dom'
 import styles from './Login.module.css'
 function Login() {
-    const [message,setMessage] = useState([])
-    /*useEffect(function(){
-        fetch('/hello')
-            .then(function(response){
-                return response.json();
-            })
-            .then(function(data){
-                setMessage(data);
-            })
-    },[])*/
 
+    const navigate = useNavigate()
+    const [loginData, setLoginData] = useState({
+        email:'',
+        pw:'',
+    })
+    const {email, pw} = loginData
+    const onSubmit = function(e){
+        e.preventDefault();
+
+        if(!email || !pw ){
+            return alert("Please add all fields")
+        }
+        const userData = {email:email, pw:pw}
+        fetch('/api/members/login',{
+            //폼 데이터 fetch 
+            method:'POST',
+                headers:{
+                    "Content-Type": "application/json",
+                },
+                body: JSON.stringify(userData),
+        }).then((response) => {
+            if(response.status == 200){
+                navigate('/home')
+            }
+            else{
+                alert(`오류 발생 Status : ${response.status}`)
+            }
+        })
+        
+    }
+    const onChange = function(e){
+        setLoginData((prevState)=>({
+            ...prevState,
+            [e.target.name]:e.target.value,
+        }))
+    }
   return (
       <>
       <div className={styles.bg}>
-      <div className='container'>
-        <form >
+      <div className={styles.container}>
+        <form className={styles.form} onSubmit={onSubmit}>
+            <div className={styles.title}>
+            <h1><font color="red">R</font>un-<font color="red">T</font>ogether</h1>
+            </div>
             <h2>Email address</h2>
-            <input className='loginInput' type="email" name='email' placeholder='enter your email'/>
+            <input className={styles.box} type="email" name='email' placeholder='Enter your email' onChange={onChange}/>
             <h2>Password</h2>
-            <input className='loginInput' type="password" name='password' placeholder='enter your password' />
-            <div >
-            <button type='submit' className='btn loginbtn'>로그인</button>
-            </div>
+            <input className={styles.box} type="password" name='pw' placeholder='Enter your password' onChange={onChange}/>
+
+            <button type='submit' className={styles.btn}>로그인</button>
             <Link to='/register'>
-            <div >
-            <button className='btn registerbtn'>회원가입</button>
-            </div>
+            <button className={styles.btn}>회원가입</button>
             </Link>
         </form>
       </div>
