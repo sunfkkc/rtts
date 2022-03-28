1de700557eef797ebcb4f948e9c6f01cee618b17
sunfkkc <rlcks77@nate.com>
2022-03-29 01:16:21 +0900

    run

--- rtts/src/pages/Run.tsx
+++ rtts/src/pages/Run.tsx
@@ -1,8 +1,17 @@
 import React from 'react'
-
+import Header from '../components/Header'
+import styles from './Run.module.css'
+import { Wrapper, Status } from "@googlemaps/react-wrapper";
 function Run() {
   return (
-    <div>Run</div>
+    <>
+      <Header />
+      <div className='body'>
+        <h1 className={styles.h1}>Run-Together</h1>
+        <div className={styles.container}>
+        </div>
+      </div>
+    </>
   )
 }
 
