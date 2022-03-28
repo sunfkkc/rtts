21bc4d9a517afcdccb6b63f12196e8481354fb20
sunfkkc <rlcks77@nate.com>
2022-03-28 16:48:52 +0900

    front

---
+++ rtts/src/pages/Login.tsx
@@ -0,0 +1,77 @@
+import React ,{useState, useEffect}from 'react'
+import {Link, useNavigate} from 'react-router-dom'
+import styles from './Login.module.css'
+interface longinData{
+    email:string,
+    pw:string,
+}
+
+function Login() {
+
+    const navigate = useNavigate()
+    const [loginData, setLoginData] = useState<longinData>({
+        email:'',
+        pw:'',
+    })
+    const {email, pw} = loginData
+    const onSubmit = function(e:React.FormEvent){
+        e.preventDefault();
+
+        if(!email || !pw ){
+            return alert("Please add all fields")
+        }
+        const userData = {email:email, pwd:pw}
+
+        
+        fetch('/api/member/login',{
+            //폼 데이터 fetch 
+            method:'POST',
+                headers:{
+                    "Content-Type": "application/json",
+                },
+                body: JSON.stringify(userData),
+        }).then((response) => {
+            if(response.ok){
+                navigate('/home')
+            }
+            else{
+                alert(`오류 발생 Status : ${response.status}`)
+            }
+        })
+        
+    }
+    const onChange = function(e:React.ChangeEvent<HTMLInputElement>){
+        setLoginData(prevState=>({
+            ...prevState,
+            [e.target.name]:e.target.value,
+        }))
+    }
+  return (
+      <>
+      <div className={styles.bg}>
+      <div className={styles.container}>
+        <form className={styles.form} onSubmit={onSubmit}>
+            <div className={styles.title}>
+                <h1>Run-Together</h1>
+            </div>
+            <h2>Email address</h2>
+            <input className={styles.box} type="email" name='email' placeholder='Enter your email' onChange={onChange}/>
+            <h2>Password</h2>
+            <input className={styles.box} type="password" name='pw' placeholder='Enter your password' onChange={onChange}/>
+            <div className='margin'>
+                <button type='submit' className={styles.btn}>로그인</button>
+            </div>
+            <div>
+                <Link to='/register'>
+                <button className={styles.btn}>회원가입</button>
+                </Link>
+            </div>
+            
+        </form>
+      </div>
+      </div>
+</>
+  )
+}
+
+export default Login
---
+++ rtts/src/pages/Register.tsx
@@ -0,0 +1,85 @@
+import React,{useState, useEffect} from 'react'
+import styles from './Register.module.css'
+import {useNavigate} from 'react-router-dom'
+
+interface formData{
+    name:string,
+    email:string,
+    pw:string,
+    password2:string,
+    gender: 'M' | 'F',
+}
+
+function Register() {
+    const navigate = useNavigate()
+    const [user,setUser] = useState<formData>({
+        name:'',
+        email:'',
+        pw:'',
+        password2:'',
+        gender:'M',
+    })
+    const {name, email, pw, password2, gender} = user
+
+    const onChange = function(e:React.ChangeEvent<HTMLInputElement>){
+        setUser((prevState) => ({
+            ...prevState,
+            [e.target.name]: e.target.value,
+        }))
+    }
+
+    const onSubmit = function(e:React.FormEvent){
+        e.preventDefault();
+        
+        if(!name || !email || !pw || !password2 || !gender){
+            return alert("Please add all fields")
+        }
+
+
+        if (pw !== password2){
+            alert('비밀번호가 일치하지 않습니다')
+            //setUser({name:'',email:'',pw:'',password2:'',})
+        }
+        else{
+            const userData = {name:name,email:email,pwd:pw,gender:gender}
+            
+            fetch('/api/member/join',{
+                method:'POST',
+                headers:{
+                    "Content-Type": "application/json",
+                },
+                body: JSON.stringify(userData),
+            }).then((response) => {
+                if(response.ok){
+                    alert('회원가입 성공')
+                    navigate('/')
+                }
+                else{
+                    alert(`오류 발생 Status : ${response.status}`)
+                }
+            })
+            .catch((error) => {alert(error.message);})
+            
+        }
+    }
+  return (
+      <>
+      <div className={styles.bg}>
+          <div className={styles.container}>
+            <form className={styles.form} onSubmit={onSubmit}>
+                <h2>Register</h2>
+                <input className={styles.box} type='text' placeholder='Enter your name' name='name' value={user.name} onChange={onChange}/>
+                <input className={styles.box} type='email' placeholder='Enter your email' name='email' value={user.email} onChange={onChange}/>
+                <input className={styles.box} type='password' placeholder='Enter your password' name='pw' value={user.pw} onChange={onChange}/>
+                <input className={styles.box} type='password' placeholder='Check your password' name='password2' value={user.password2} onChange={onChange}/>
+                <div className={styles.radio}><input checked type='radio' name='gender' value='M' onChange={onChange}/><label>남성</label></div>
+                <div className={styles.radio}><input type='radio' name='gender' value='F' onChange={onChange}/><label>여성</label></div>
+                <button type='submit' className={styles.btn} >Register</button>
+            </form>
+          </div>
+      </div>
+      </>
+  )
+}
+
+export default Register
