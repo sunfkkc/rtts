10da607540c631b2cdb55cdb58b30ae3d54b5222
sunfkkc <rlcks77@nate.com>
2022-03-30 01:18:24 +0900

    runstate

--- rtts/src/pages/Run.tsx
+++ rtts/src/pages/Run.tsx
@@ -1,24 +1,21 @@
-import React,{useEffect}  from 'react'
+import React, { useEffect } from 'react'
 import Header from '../components/Header'
+import Map from './Map'
 import styles from './Run.module.css'
+import { runState } from './runState'
+import { useRecoilState } from 'recoil'
+
 function Run() {
-    const {kakao}:any = window
-    useEffect(function(){
-        const container = document.getElementById('map'); //지도를 담을 영역의 DOM 레퍼런스
-        const options = { //지도를 생성할 때 필요한 기본 옵션
-            center: new kakao.maps.LatLng(33.450701, 126.570667), //지도의 중심좌표.
-            level: 3 //지도의 레벨(확대, 축소 정도)
-        };
-        
-        const map = new kakao.maps.Map(container, options); //지도 생성 및 객체 리턴
-    })
+  const [runData, setRunData] = useRecoilState(runState)
+
   return (
     <>
       <Header />
       <div className='body'>
         <h2 className={styles.h1}>Run-Together</h2>
         <div className={styles.container}>
-        <div id="map" className={styles.map}></div>
+          <h3>주행거리</h3>{runData.dis}
+          <Map />
         </div>
       </div>
     </>
--- rtts/src/pages/runState.ts
+++ rtts/src/pages/runState.ts
@@ -1,16 +1,16 @@
-import {atom} from 'recoil'
+import { atom } from 'recoil'
 
-interface runState{
-    dis:number,
-    speed:number,
-    time:number,
+interface runState {
+    dis: number,
+    speed: number,
+    time: number,
 }
 
-const runState = atom<runState>({
-    key:'runState',
-    default:{
-        dis:0,
-        speed:0,
-        time:0,
+export const runState = atom<runState>({
+    key: 'runState',
+    default: {
+        dis: 0,
+        speed: 0,
+        time: 0,
     }
 })
